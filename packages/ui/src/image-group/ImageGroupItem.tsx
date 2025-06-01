9eb760dc40d0341a0be52efd44ea22f4ba005aef
Matthias Epheser-Andrä <matthias.epheser@indoqa.com>
2025-06-01 21:43:39 +0200

    feat(website): test anchors in works

--- packages/ui/src/image-group/ImageGroupItem.tsx
+++ packages/ui/src/image-group/ImageGroupItem.tsx
@@ -38,14 +38,8 @@ const ImageGroupItem: FunctionComponent<Props> = ({data, context, isSelected}) =
     window.scroll({top})
   }, [isSelected])
 
-  const toggle = useCallback(() => {
-    if (context) {
-      router.push(href(context, data, isSelected), {scroll: false})
-    }
-  }, [context, isSelected, data])
-
   return (
-    <a onClick={toggle}>
+    <a href={context ? href(context, data, isSelected) : '#'}>
       <div className={'group relative h-[235px] w-[300px] cursor-pointer'} ref={ref}>
         <ImageGroupPreviewImage image={data.image} />
         {(context === undefined || context.activeSubcategory!.id === 'studio') && <ImageGroupItemLabel data={data} />}
