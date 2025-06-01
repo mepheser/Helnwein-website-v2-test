8096ca65746ce1fe86e55714825d4540975f2b62
Matthias Epheser-Andrä <matthias.epheser@indoqa.com>
2025-06-01 21:00:44 +0200

    mend

--- apps/website/src/app/[site]/[category]/[subcategory]/layout.tsx
+++ apps/website/src/app/[site]/[category]/[subcategory]/layout.tsx
@@ -4,14 +4,15 @@ import Submenu from '@repo/ui/menu/Submenu'
 
 interface Props {
   children: React.ReactNode
-  params: {
+  params: Promise<{
     category: string
     subcategory: string
-  }
+  }>
 }
 
-const SubCategoryLayout: FunctionComponent<Props> = ({children, params}) => {
-  const categoryContext = getCategoryContext(params)
+const SubCategoryLayout: FunctionComponent<Props> = async ({children, params}) => {
+  const awaitedParams = await params
+  const categoryContext = getCategoryContext(awaitedParams)
 
   if (!categoryContext.activeCategory || !categoryContext.activeSubcategory) {
     return null
