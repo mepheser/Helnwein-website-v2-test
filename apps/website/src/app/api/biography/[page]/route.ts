cc08504842a15b34066d3def5dbe335e85918375
Matthias Epheser-Andrä <matthias.epheser@indoqa.com>
2025-08-24 19:24:00 +0200

    feat(website): add infinite scroll #2

--- apps/website/src/app/api/biography/[page]/route.ts
+++ apps/website/src/app/api/biography/[page]/route.ts
@@ -1,6 +1,6 @@
 import {getBiographyList} from '@repo/sanity/queries'
 
-export async function GET() {
+export async function GET(request: Request, {params}: any) {
   const data = await getBiographyList(params.page)
   return Response.json({data})
 }
